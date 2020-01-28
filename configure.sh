7f38ca4cdad0e5a3f272b2760dd48f20976da464
fg14 <filip.geppert@gmail.com>
2020-01-28 10:42:06 +0100

    Remove git clone

--- configure.sh
+++ configure.sh
@@ -5,7 +5,6 @@ TOP_DIRECTORY="omip_dl"
 BUCKET_NAME="omip-images"
 
 sudo apt-get update
-git clone https://github.com/filipgeppert/omip_dl.git
 aws s3 cp s3://$BUCKET_NAME/images/ ~/$TOP_DIRECTORY/images/ --recursively
 source activate pytorch_p36
 conda install pandas
